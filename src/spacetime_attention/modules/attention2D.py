2366860fea14afdb04f914274e32b248186077ec
apandy02 <aryaman.pandya@tufts.edu>
2025-06-26 11:50:07 +0100

    first commit - beginning attention implementation

---
+++ src/spacetime_attention/modules/attention2D.py
@@ -0,0 +1,199 @@
+"""
+This module contains the building blocks for a 2D attention mechanism.
+Author: Aryaman Pandya
+"""
+
+import math
+from typing import Optional
+
+import torch
+import torch.nn as nn
+
+
+def scaled_dot_product_attention(
+        q: torch.Tensor,
+        k: torch.Tensor,
+        d_k: int,
+        mask: torch.Tensor
+) -> torch.Tensor:
+    """
+    Scaled dot product attention.
+
+    Args:
+        q: query tensor
+        k: key tensor
+        d_k: dimension of the key
+        mask: whether to use a mask
+
+    Returns:
+        attention: attention tensor
+    """
+    scores = torch.matmul(q, k.transpose(-2, -1)) / math.sqrt(d_k)
+    if mask:
+        mask = torch.tril(torch.ones(scores.shape)).to(q.device)
+        scores = scores.masked_fill(mask == 0, float('-inf'))
+    return nn.Softmax(-1)(scores)
+
+
+class Attention2D(nn.Module):
+    """
+    Multihead attention.
+    """
+    def __init__(
+        self,
+        dropout: float,
+        num_heads: int,
+        num_channels: int,
+        num_groups: int = 8,
+        d_k: Optional[int] = None,
+        mask: bool = False
+    ):
+        """
+        TODO: change to ViT style, expecting patches
+
+        Args:
+            d_k: dimension of the key
+            dropout: dropout rate
+            num_heads: number of heads
+            num_channels: number of channels
+            num_groups: number of groups for group normalization
+            mask: whether to use a mask
+        """
+        super(Attention2D, self).__init__()
+        self.d_k = d_k if d_k is not None else num_channels
+        self.num_heads = num_heads
+
+        self.query_projection = nn.Linear(num_channels, num_heads * self.d_k)
+        self.key_projection = nn.Linear(num_channels, num_heads * self.d_k)
+        self.value_projection = nn.Linear(num_channels, num_heads * self.d_k)
+
+        self.group_norm = nn.GroupNorm(
+            num_groups=num_groups, 
+            num_channels=num_channels
+        )
+        self.output_layer = nn.Linear(num_heads * self.d_k, num_channels)
+        self.dropout = nn.Dropout(dropout)
+        self.mask = mask
+        self.num_channels = num_channels
+
+    def forward(self, x: torch.Tensor, y: Optional[torch.Tensor] = None) -> torch.Tensor:
+        """
+        forward pass for 2D attention.
+
+        Args:
+            x: input tensor
+            y: optional tensor for cross-attention
+        """
+        batch_size, n_channels, height, width = x.shape
+        x = self.group_norm(x)
+        x = x.view(batch_size, n_channels, height * width).permute(0, 2, 1)
+
+        residual = x
+
+        if y is not None:
+            k, q, v = y, x, y
+        else:
+            k, q, v = x, x, x
+
+        k_len, q_len, v_len = k.size(1), q.size(1), v.size(1)
+
+        k = self.key_projection(k).view(batch_size, k_len, self.num_heads, self.d_k)
+        q = self.query_projection(q).view(batch_size, q_len, self.num_heads, self.d_k)
+        v = self.value_projection(v).view(batch_size, v_len, self.num_heads, self.d_k)
+
+        attention = scaled_dot_product_attention(
+            q.transpose(1, 2),
+            k.transpose(1, 2),
+            self.d_k, 
+            self.mask
+        )
+        output = torch.matmul(attention, v.transpose(1, 2))
+        output = self.output_layer(output.transpose(1, 2).contiguous().view(batch_size, q_len, -1))
+
+        h = self.dropout(output) + residual
+
+        h = h.permute(0, 2, 1).view(batch_size, n_channels, height, width)
+
+        return h
+
+
+class STAttention(nn.Module):
+    """
+    Spatial-temporal attention.
+
+    TODO: currently just a copy of the 2D attention.
+    """
+    def __init__(
+        self,
+        dropout: float,
+        num_heads: int,
+        num_channels: int,
+        num_groups: int = 8,
+        d_k: Optional[int] = None,
+        mask: bool = False
+    ):
+        """
+        Args:
+            d_k: dimension of the key
+            dropout: dropout rate
+            num_heads: number of heads
+            num_channels: number of channels
+            num_groups: number of groups for group normalization
+            mask: whether to use a mask
+        """
+        super(Attention2D, self).__init__()
+        self.d_k = d_k if d_k is not None else num_channels
+        self.num_heads = num_heads
+
+        self.query_projection = nn.Linear(num_channels, num_heads * self.d_k)
+        self.key_projection = nn.Linear(num_channels, num_heads * self.d_k)
+        self.value_projection = nn.Linear(num_channels, num_heads * self.d_k)
+
+        self.group_norm = nn.GroupNorm(
+            num_groups=num_groups, 
+            num_channels=num_channels
+        )
+        self.output_layer = nn.Linear(num_heads * self.d_k, num_channels)
+        self.dropout = nn.Dropout(dropout)
+        self.mask = mask
+        self.num_channels = num_channels
+
+    def forward(self, x: torch.Tensor, y: Optional[torch.Tensor] = None) -> torch.Tensor:
+        """
+        forward pass for 2D attention.
+
+        Args:
+            x: input tensor
+            y: optional tensor for cross-attention
+        """
+        batch_size, n_channels, time_steps, height, width = x.shape
+        x = self.group_norm(x)
+        x = x.view(batch_size, n_channels, height * width).permute(0, 2, 1)
+
+        residual = x
+
+        if y is not None:
+            k, q, v = y, x, y
+        else:
+            k, q, v = x, x, x
+
+        k_len, q_len, v_len = k.size(1), q.size(1), v.size(1)
+
+        k = self.key_projection(k).view(batch_size, k_len, self.num_heads, self.d_k)
+        q = self.query_projection(q).view(batch_size, q_len, self.num_heads, self.d_k)
+        v = self.value_projection(v).view(batch_size, v_len, self.num_heads, self.d_k)
+
+        attention = scaled_dot_product_attention(
+            q.transpose(1, 2),
+            k.transpose(1, 2),
+            self.d_k, 
+            self.mask
+        )
+        output = torch.matmul(attention, v.transpose(1, 2))
+        output = self.output_layer(output.transpose(1, 2).contiguous().view(batch_size, q_len, -1))
+
+        h = self.dropout(output) + residual
+
+        h = h.permute(0, 2, 1).view(batch_size, n_channels, height, width)
+
+        return h
---
+++ src/spacetime_attention/modules/resnet.py
@@ -0,0 +1,154 @@
+"""
+This module contains the building blocks for a ResNet model.
+Author: Aryaman Pandya
+"""
+
+import torch.nn as nn
+import torch
+from typing import Optional
+
+
+class ResBlock(nn.Module):
+    """
+    A residual block with optional timestep embedding handling.
+    Can be used as a building block for a ResNet or a UNet used
+    for diffusion.
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        activation: nn.Module,
+        kernel_size: int = 3,
+        stride: int = 1,
+        padding: int = 1,
+        dropout: float = 0.2,
+        num_groups: int = 1,
+        timestep_emb_dim: Optional[int] = None,
+    ):
+        """
+        Args:
+            in_channels: number of input channels
+            out_channels: number of output channels
+            activation: activation function
+            kernel_size: convolutional kernel size
+            stride: convolutional stride
+            padding: padding
+            dropout: dropout rate
+            num_groups: number of groups for group normalization
+            timestep_emb_dim: number of dimensions in the timestep embedding
+        """
+        super().__init__()
+        self.norm1 = nn.Sequential(
+            nn.GroupNorm(num_groups=num_groups, num_channels=in_channels), activation()
+        )
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                out_channels,
+                out_channels,
+                kernel_size=kernel_size,
+                stride=1,
+                padding=padding,
+            ),
+            nn.GroupNorm(num_groups=num_groups, num_channels=out_channels),
+        )
+        self.activation = activation()
+
+        if in_channels == out_channels:
+            self.idconv = nn.Identity()
+        else:
+            self.idconv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1)
+
+        self.avgpool = (
+            nn.Identity() if stride == 1 else nn.AvgPool2d(kernel_size=2, stride=stride)
+        )
+        self.activation = activation()
+        self.dropout = nn.Dropout(dropout)
+        self.timestep_emb_dim = timestep_emb_dim
+
+        if self.timestep_emb_dim is not None:
+            self.timestep_emb_proj = nn.Linear(timestep_emb_dim, out_channels)
+
+    def forward(self, x, timestep_emb=None):
+        """
+        Forward pass of the residual block.
+        Args:
+            x: inpuwt tensor
+            timestep_emb: optional timestep embedding
+        """
+        h = self.norm1(x)
+        h = self.conv1(h)
+
+        if self.timestep_emb_dim is not None:
+            timestep_emb = self.timestep_emb_proj(timestep_emb)[
+                :, :, None, None
+            ]  # Enables broadcasting
+            h += self.activation(timestep_emb)
+
+        h = self.conv2(h)
+        h_id = self.avgpool(self.idconv(x))
+
+        return self.dropout(self.activation(h + h_id))
+
+
+class ResNet(nn.Module):
+    """
+    A simple ResNet model with a series of residual blocks.
+    """
+
+    def __init__(
+        self,
+        num_channels: int,
+        num_classes: int,
+        filters: list[int],
+        activation: nn.Module,
+        stride: int = 2,
+        dropout: float = 0.2,
+    ):
+        """
+        Args:
+            num_channels: number of input channels
+            num_classes: number of output classes
+            filters: list of filter sizes
+            activation: activation function
+            stride: convolutional stride
+            dropout: dropout rate
+        """
+        super().__init__()
+        res_layers = [ResBlock(num_channels, filters[0], activation, stride=1)]
+        for i in range(len(filters) - 1):
+            res_layers += [
+                ResBlock(
+                    filters[i],
+                    filters[i + 1],
+                    activation,
+                    stride=stride,
+                    dropout=dropout,
+                )
+            ]
+        self.res_layers = nn.Sequential(*res_layers)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.flatten = nn.Flatten()
+        self.linear = nn.Sequential(
+            nn.Linear(filters[-1], num_classes, bias=False),
+            nn.BatchNorm1d(num_classes),
+            activation(),
+        )
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x: torch.Tensor):
+        for _, layer in enumerate(self.res_layers):
+            x = layer(x)
+
+        h = self.avgpool(x)
+        h = self.flatten(h)
+        h = self.dropout(h)
+        return self.linear(h)
