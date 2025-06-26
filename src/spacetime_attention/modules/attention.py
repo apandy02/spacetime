5085b5d5b8eb75b40b4f71654134848130f954dd
apandy02 <aryaman.pandya@tufts.edu>
2025-06-26 14:09:38 +0100

    building spatio temporal attention block

--- src/spacetime_attention/modules/attention.py
+++ src/spacetime_attention/modules/attention.py
@@ -35,7 +35,7 @@ def scaled_dot_product_attention(
     return nn.Softmax(-1)(scores)
 
 
-class Attention2D(nn.Module):
+class Attention(nn.Module):
     """
     Multihead attention.
     """
@@ -59,7 +59,7 @@ class Attention2D(nn.Module):
             num_groups: number of groups for group normalization
             mask: whether to use a mask
         """
-        super(Attention2D, self).__init__()
+        super(Attention, self).__init__()
         self.d_k = d_k if d_k is not None else num_channels
         self.num_heads = num_heads
 
@@ -68,7 +68,7 @@ class Attention2D(nn.Module):
         self.value_projection = nn.Linear(num_channels, num_heads * self.d_k)
 
         self.group_norm = nn.GroupNorm(
-            num_groups=num_groups, 
+            num_groups=num_groups,
             num_channels=num_channels
         )
         self.output_layer = nn.Linear(num_heads * self.d_k, num_channels)
@@ -84,10 +84,8 @@ class Attention2D(nn.Module):
             x: input tensor
             y: optional tensor for cross-attention
         """
-        batch_size, n_channels, height, width = x.shape
+        batch_size, _, _, _ = x.shape
         x = self.group_norm(x)
-        x = x.view(batch_size, n_channels, height * width).permute(0, 2, 1)
-
         residual = x
 
         if y is not None:
@@ -104,22 +102,18 @@ class Attention2D(nn.Module):
         attention = scaled_dot_product_attention(
             q.transpose(1, 2),
             k.transpose(1, 2),
-            self.d_k, 
+            self.d_k,
             self.mask
         )
         output = torch.matmul(attention, v.transpose(1, 2))
         output = self.output_layer(output.transpose(1, 2).contiguous().view(batch_size, q_len, -1))
 
-        h = self.dropout(output) + residual
-
-        h = h.permute(0, 2, 1).view(batch_size, n_channels, height, width)
-
-        return h
+        return self.dropout(output) + residual
 
 
 class STAttention(nn.Module):
     """
-    Spatial-temporal attention.
+    Spatial-temporal attention block, as implemented in TimeSformer (Bertasius et al., 2020).
 
     TODO: currently just a copy of the 2D attention.
     """
@@ -130,7 +124,6 @@ class STAttention(nn.Module):
         num_channels: int,
         num_groups: int = 8,
         d_k: Optional[int] = None,
-        mask: bool = False
     ):
         """
         Args:
@@ -141,59 +134,27 @@ class STAttention(nn.Module):
             num_groups: number of groups for group normalization
             mask: whether to use a mask
         """
-        super(Attention2D, self).__init__()
-        self.d_k = d_k if d_k is not None else num_channels
-        self.num_heads = num_heads
-
-        self.query_projection = nn.Linear(num_channels, num_heads * self.d_k)
-        self.key_projection = nn.Linear(num_channels, num_heads * self.d_k)
-        self.value_projection = nn.Linear(num_channels, num_heads * self.d_k)
-
-        self.group_norm = nn.GroupNorm(
-            num_groups=num_groups, 
-            num_channels=num_channels
+        super(STAttention, self).__init__()
+        self.spatial_attention = Attention(
+            dropout=dropout,
+            num_heads=num_heads,
+            num_channels=num_channels,
+            num_groups=num_groups,
+            d_k=d_k,
+            mask=False,
         )
-        self.output_layer = nn.Linear(num_heads * self.d_k, num_channels)
-        self.dropout = nn.Dropout(dropout)
-        self.mask = mask
-        self.num_channels = num_channels
+
+        self.temporal_attention = Attention(
+            dropout=dropout,
+            num_heads=num_heads,
+            num_channels=num_channels,
+            num_groups=num_groups,
+            d_k=d_k,
+            mask=True,  # since temporal attention is causal
+        )
 
     def forward(self, x: torch.Tensor, y: Optional[torch.Tensor] = None) -> torch.Tensor:
         """
-        forward pass for 2D attention.
-
-        Args:
-            x: input tensor
-            y: optional tensor for cross-attention
+        forward pass for spatial-temporal attention.
         """
-        batch_size, n_channels, time_steps, height, width = x.shape
-        x = self.group_norm(x)
-        x = x.view(batch_size, n_channels, height * width).permute(0, 2, 1)
-
-        residual = x
-
-        if y is not None:
-            k, q, v = y, x, y
-        else:
-            k, q, v = x, x, x
-
-        k_len, q_len, v_len = k.size(1), q.size(1), v.size(1)
-
-        k = self.key_projection(k).view(batch_size, k_len, self.num_heads, self.d_k)
-        q = self.query_projection(q).view(batch_size, q_len, self.num_heads, self.d_k)
-        v = self.value_projection(v).view(batch_size, v_len, self.num_heads, self.d_k)
-
-        attention = scaled_dot_product_attention(
-            q.transpose(1, 2),
-            k.transpose(1, 2),
-            self.d_k, 
-            self.mask
-        )
-        output = torch.matmul(attention, v.transpose(1, 2))
-        output = self.output_layer(output.transpose(1, 2).contiguous().view(batch_size, q_len, -1))
-
-        h = self.dropout(output) + residual
-
-        h = h.permute(0, 2, 1).view(batch_size, n_channels, height, width)
-
-        return h
+        return torch.Tensor(0)  # TODO: implement
