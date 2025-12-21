1f52643edf8dadde0d4d9cf8318f4763077f9a95
apandy02 <aryaman.pandya@tufts.edu>
2025-12-21 22:19:07 +0000

    completed implementing dynamics model

--- src/spacetime/models/dynamics_model.py
+++ src/spacetime/models/dynamics_model.py
@@ -11,41 +11,46 @@ class DynamicsModel(nn.Module):
     """
     def __init__(
         self,
-        num_heads: int,
+        n_heads: int,
         d_model: int,
-        num_layers: int,
+        n_layers: int,
         d_linear: int,
         action_codebook_size: int,
         action_dim: int,
         token_codebook_size: int,
         token_dim: int,
-        num_tokens: int,
-        num_linear_layers: int = 2,
-        num_groups: int = 8,
+        n_tokens: int,
+        p_sample: float = 0.2,
+        n_linear_layers: int = 2,
+        n_groups: int = 8,
         dropout: float = 0.1,
-        num_frames: int = 16,
+        n_frames: int = 16,
     ):
         super(DynamicsModel, self).__init__()
-        self.n_tokens, self.num_frames = num_tokens, num_frames
+        self.n_tokens, self.n_frames = n_tokens, n_frames
+        self.d_model = d_model
+        self.p_sample = p_sample
         self.action_codebook_size, self.token_codebook_size = action_codebook_size, token_codebook_size
         self.st_decoder = nn.ModuleList(
             [
                 STTransformerLayer(
-                    num_heads,
+                    n_heads,
                     d_model,
                     d_linear,
-                    num_linear_layers,
-                    num_groups,
+                    n_linear_layers,
+                    n_groups,
                     dropout,
                     mask=build_causal_mask,
                 )
-                for _ in range(num_layers)
+                for _ in range(n_layers)
             ]
         )
         self.layer_norm = nn.LayerNorm(d_model)
 
         self.pos_embed_space = nn.Parameter(torch.zeros(1, 1, self.n_tokens, d_model))
-        self.pos_embed_time = nn.Parameter(torch.zeros(1, self.num_frames, 1, d_model))
+        self.pos_embed_time = nn.Parameter(torch.zeros(1, self.n_frames, 1, d_model))
+
+        self.mask_embed = nn.Parameter(torch.zeros(d_model))
 
         self.action_projector = nn.Linear(action_dim, d_model)
         self.token_projector = nn.Linear(token_dim, d_model)
@@ -62,11 +67,11 @@ class DynamicsModel(nn.Module):
 
         tokens = self._mask(tokens)
 
-         for layer in self.st_decoder:
-            x = layer(x)
+        for layer in self.st_decoder:
+            tokens = layer(tokens)
         
-        x = self.layer_norm(x)
-        return x
+        tokens = self.layer_norm(tokens)
+        return tokens
 
 
     
@@ -75,4 +80,8 @@ class DynamicsModel(nn.Module):
         """
         Bernoulli masking 
         """
+        batch_size, n_frames, n_tokens, _ = tokens.shape[0]
+        a = torch.empty((batch_size, n_frames, n_tokens)).uniform_(0, 1)
+        mask = a < self.p_sample
+        tokens = torch.where(mask[..., None], self.mask_embed, tokens)
         return tokens
